ddd9d1a41ee072f62b5627842dcf6af6a4b51aba
interdimensionalpizza <michaellake05@gmail.com>
2023-08-18 12:03:36 -0700

    changed settings before deploying website

--- backend/game_research_proj/settings.py
+++ backend/game_research_proj/settings.py
@@ -25,13 +25,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173"
-]
+CORS_ALLOW_ALL_ORIGINS = True
+
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:5173"
+# ]
 
 # Application definition
 
